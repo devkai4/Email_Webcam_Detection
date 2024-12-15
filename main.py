1ab02fe336c5aa5eba830a1fea63ba0feecd8062
devkai4 <devkai4@pm.me>
2024-12-15 10:37:43 +0900

    Extarct Images from Webcam Video

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import cv2
 import time
+import glob
 from emailing import send_email
 
 from emailing import send_email
@@ -9,6 +10,7 @@ time.sleep(1)
 
 first_frame = None
 status_list = []
+count = 1
 
 while True:
     status = 0
@@ -36,6 +38,11 @@ while True:
         rectangle = cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
         if rectangle.any():
             status = 1
+            cv2.imwrite(f"images/{count}.png", frame)
+            count = count + 1
+            all_image = glob.glob("images/*.png")
+            index = int(len(all_images) / 2)
+            image_with_object = all_images[index]
 
 
     status_list.append(status)
@@ -53,4 +60,4 @@ while True:
     if key == ord("q"):
         break
 
-video.realease()
+video.release()
